7a1ee2009642835e91721b072e6e582f7f88fa45
MuhammadShihata <mushihata@gmail.com>
2020-05-09 04:16:27 +0200

    back to filesystem... memchashed need to add a lot of steps as mentioned in heroku devcenter

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "memcached"
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
